8e3cc13a311573e445716ab1aa977b98f0c525ab
Personal <massimovenezia@Massimos-MBP.bbrouter>
2024-07-06 22:09:27 -0300

    Updated Image

--- src/App.js
+++ src/App.js
@@ -7,18 +7,18 @@ function App() {
   };
 
   return (
-    <div className="container">
-      <div className="top-left" onClick={() => handleClick('tel:+541126510620')}></div>
-      <div className="top-right">
-        <ul>
-          <li onClick={() => handleClick('https://instagram.com/massiveneziaa')}></li>
-          <li onClick={() => handleClick('https://x.com/massivenezia')}></li>
-          <li onClick={() => handleClick('https://linkedin.com/in/massivenezia')}></li>
-        </ul>
-      </div>
-      <div className="center" onClick={() => handleClick('https://example.com/center')}></div>
-      <div className="bottom-center" onClick={() => handleClick('mailto:contact@massimovenezia.com')}></div>
+  <div className="container">
+    <div className="clickable top-left" onClick={() => handleClick('tel:+541126510620')}></div>
+    <div className="clickable top-right">
+      <ul>
+        <li onClick={() => handleClick('https://instagram.com/massiveneziaa')}></li>
+        <li onClick={() => handleClick('https://x.com/massivenezia')}></li>
+        <li onClick={() => handleClick('https://linkedin.com/in/massivenezia')}></li>
+      </ul>
     </div>
+    <div className="clickable center" onClick={() => handleClick('https://example.com/center')}></div>
+    <div className="clickable bottom-center" onClick={() => handleClick('mailto:contact@massimovenezia.com')}></div>
+  </div>
   );
 }
 
