5b7422682725e3cb66ab3f9aab7d221ec5292641
Personal <massimovenezia@Massimos-MBP.bbrouter>
2024-07-07 16:52:28 -0300

    Finished

--- src/SocialMedia.js
+++ src/SocialMedia.js
@@ -1,17 +1,25 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import './SocialMedia.css';
 
 function SocialMedia() {
+  const navigate = useNavigate();
+
   const handleClick = (url) => {
     window.open(url, '_blank');
   };
 
+  const handleNavigate = (path) => {
+    navigate(path);
+  };
+
   return (
-  <div className="container">
-    <div className="clickable x" onClick={() => handleClick('https://x.com/massivenezia')}></div>
-    <div className="clickable instagram" onClick={() => handleClick('https://instagram.com/massiveneziaa')}></div>
-    <div className="clickable linkedin" onClick={() => handleClick('https://linkedin.com/in/massivenezia')}></div>
-  </div>
+    <div className="container2">
+      <div className="clickable2 x" onClick={() => handleClick('https://x.com/massivenezia')}></div>
+      <div className="clickable2 instagram" onClick={() => handleClick('https://instagram.com/massiveneziaa')}></div>
+      <div className="clickable2 linkedin" onClick={() => handleClick('https://linkedin.com/in/massivenezia')}></div>
+      <div className="clickable2 info" onClick={() => handleNavigate('/')}></div>
+    </div>
   );
 }
 
